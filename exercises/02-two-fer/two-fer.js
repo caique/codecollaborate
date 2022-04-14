5ecfc6d4ce07cf07e9ecfdf41bd96a81a53a0f68
Caique Peixoto <caique.peixoto@futurice.com>
2022-04-14 15:03:01 +0200

    Solution for 02

--- exercises/02-two-fer/two-fer.js
+++ exercises/02-two-fer/two-fer.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`;
 };
