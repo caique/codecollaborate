850e8432cefa9517fec70a4e710fe788c9198c53
Caique Peixoto <caique.peixoto@futurice.com>
2022-04-14 15:03:10 +0200

    Solution for 03

--- exercises/03-roman-numerals/roman-numerals.js
+++ exercises/03-roman-numerals/roman-numerals.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRoman = (number) => {
+  const decimalValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const romanNumeral = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  
+  let inRoman = "";
+
+  for(let i = 0; i < decimalValue.length; i++) {
+    while(number >= decimalValue[i]) {
+      inRoman += romanNumeral[i];
+      number -= decimalValue[i];
+    }
+  }
+
+  return inRoman;
 };
